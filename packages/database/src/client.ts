251392e635f7560f2e95f1cbb1929ebe85bc2ba3
ToyB0x <83337042+ToyB0x@users.noreply.github.com>
2025-06-15 04:35:57 +0900

    chore: update client and seed files for improved type handling and linting

--- packages/database/src/client.ts
+++ packages/database/src/client.ts
@@ -4,6 +4,7 @@ const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
 export const prisma = globalForPrisma.prisma || new PrismaClient();
 
+// biome-ignore lint/complexity/useLiteralKeys: strictest/tsconfig.json
 if (process.env?.["NODE_ENV"] !== "production") globalForPrisma.prisma = prisma;
 
 export * from "../generated/client";
