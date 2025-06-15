14293341d89dffc9e534ef5780a701cb74d198ef
ToyB0x <83337042+ToyB0x@users.noreply.github.com>
2025-06-16 07:45:03 +0900

    feat: sort results by duration and enhance table display in showTable function

--- apps/cli/src/commands/analyze/libs/showTable.ts
+++ apps/cli/src/commands/analyze/libs/showTable.ts
@@ -4,20 +4,23 @@ export const showTable = (results: TscResult[]): void => {
   console.log("```");
   console.table(
     results
-      .sort((a, b) => a.durationMs - b.durationMs)
-      .map((result) => ({
-        ...result,
-        package: result.package.name,
-      })),
-    [
-      "package",
-      "durationMs",
-      "numTrace",
-      "numType",
-      "durationMsHotSpots",
-      "numHotSpots",
-      "isSuccess",
-    ],
+      .sort((a, b) => b.durationMs - a.durationMs)
+      .map((result) =>
+        result.isSuccess
+          ? {
+              package: result.package.name,
+              ms: result.durationMs,
+              traces: result.numTrace,
+              types: result.numType,
+              hotSpots: result.numHotSpot,
+              hotSpotMs: result.durationMsHotSpot,
+            }
+          : {
+              package: result.package.name,
+              ms: result.durationMs,
+              error: String(result.error),
+            },
+      ),
   );
   console.log("```");
 };
