bb129db5b4581555a2f705a436263b53cdbfa60d
ToyB0x <83337042+ToyB0x@users.noreply.github.com>
2025-06-15 09:32:03 +0900

    chore: standardize quotation marks in index.ts

---
+++ apps/cli/src/commands/analyze/tsc/tsc.ts
@@ -0,0 +1,7 @@
+export const tsc = () => {
+  console.log("Running tsc performance check...");
+
+  // Step 1: list packages in the git repository
+  // Step 2: run tsc for each package, and store result to sqlite (with multicore support)
+  // Step 3: analyze the result and output to stdout
+};
