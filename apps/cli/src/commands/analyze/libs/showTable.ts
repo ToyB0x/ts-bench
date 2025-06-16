3c2a9498d1ea38aaefc82f1ae39830305e29c801
ToyB0x <83337042+ToyB0x@users.noreply.github.com>
2025-06-16 11:18:18 +0900

    feat: enhance showTable function to display recent scan results and calculate averages

--- apps/cli/src/commands/analyze/libs/showTable.ts
+++ apps/cli/src/commands/analyze/libs/showTable.ts
@@ -1,26 +1,62 @@
+import { type Result, prisma } from "@repo/db";
 import type { TscResult } from "./tscAndAnalyze";
 
-export const showTable = (results: TscResult[]): void => {
+export const showTable = async (results: TscResult[]) => {
+  const recentScans = await prisma.scan.findMany({
+    take: 10,
+    orderBy: {
+      createdAt: "desc",
+    },
+    select: {
+      results: true,
+    },
+  });
+
+  const recentResults = recentScans.flatMap((scan) => scan.results);
+
   console.log("```");
   console.table(
     results
       .sort((a, b) => b.durationMs - a.durationMs)
-      .map((result) =>
-        result.isSuccess
+      .map((r) =>
+        r.isSuccess
           ? {
-              package: result.package.name,
-              ms: result.durationMs,
-              traces: result.numTrace,
-              types: result.numType,
-              hotSpots: result.numHotSpot,
-              hotSpotMs: result.durationMsHotSpot,
+              package: r.package.name,
+              ms: `${r.durationMs} (${calcDiff(calcAverage(recentResults, r.package.name, "durationMs"), r.durationMs)})`,
+              traces: `${r.numTrace} (${calcDiff(calcAverage(recentResults, r.package.name, "numTrace"), r.numTrace)})`,
+              types: `${r.numType} (${calcDiff(calcAverage(recentResults, r.package.name, "numType"), r.numType)})`,
+              hotSpots: `${r.numHotSpot} (${calcDiff(calcAverage(recentResults, r.package.name, "numHotSpot"), r.numHotSpot)})`,
+              hotSpotMs: `${r.durationMsHotSpot} (${calcDiff(calcAverage(recentResults, r.package.name, "durationMsHotSpot"), r.durationMsHotSpot)})`,
             }
           : {
-              package: result.package.name,
-              ms: result.durationMs,
-              error: String(result.error),
+              package: r.package.name,
+              ms: `${r.durationMs} (${calcDiff(calcAverage(recentResults, r.package.name, "durationMs"), r.durationMs)})`,
+              error: String(r.error),
             },
       ),
   );
   console.log("```");
+};
+
+// calculate average values for a specific package and column with given results
+const calcAverage = (
+  results: Result[],
+  packageName: Result["package"],
+  column: keyof Pick<
+    Result,
+    "numTrace" | "numType" | "numHotSpot" | "durationMs" | "durationMsHotSpot"
+  >,
+) =>
+  results
+    .filter((r) => r.package === packageName)
+    .map((r) => r[column])
+    .reduce((acc, value) => acc + value, 0) / results.length;
+
+// calculate the difference between two numbers (eg, +XX.X % or -XX.X %)
+const calcDiff = (before: number, after: number): string => {
+  if (before === 0) return "";
+
+  const diff = ((after - before) / Math.abs(before)) * 100;
+  const sign = diff > 0 ? "+" : "-";
+  return `${sign}${diff.toFixed(1)}%`;
 };
