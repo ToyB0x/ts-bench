1b78de7a09fde0946eb811f3bf24bd43ea0020f9
ToyB0x <83337042+ToyB0x@users.noreply.github.com>
2025-06-17 05:41:53 +0900

    feat: enhance routing configuration for static site generation and add database dependency

--- apps/web-monitor/react-router.config.ts
+++ apps/web-monitor/react-router.config.ts
@@ -1,7 +1,21 @@
 import type { Config } from "@react-router/dev/config";
+import { prisma } from "@repo/db";
 
 export default {
-  // Config options...
-  // Server-side render by default, to enable SPA mode set this to `false`
-  ssr: true,
+  // Deployed to a static file server
+  ssr: false,
+  // all static paths and dynamic segments like "/post/:slug"
+  async prerender({ getStaticPaths }) {
+    const packages = await prisma.result.findMany({
+      distinct: ["package"],
+      orderBy: {
+        package: "asc",
+      },
+      select: {
+        package: true,
+      },
+    });
+    const packagePaths = packages.map((pkg) => `/packages/${pkg.package}`);
+    return [...getStaticPaths(), ...packagePaths];
+  },
 } satisfies Config;
