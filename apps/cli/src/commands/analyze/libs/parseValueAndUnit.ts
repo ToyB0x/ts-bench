3f04d32a7ac940590b3ed911187031525741aa5a
ToyB0x <83337042+ToyB0x@users.noreply.github.com>
2025-06-22 19:17:31 +0900

    feat: add parseValueAndUnit function and corresponding tests

---
+++ apps/cli/src/commands/analyze/libs/parseValueAndUnit.ts
@@ -0,0 +1,17 @@
+export const parseValueAndUnit = (
+  valueStringMayHaveUnit: string,
+): { value: number; unit: string | null } => {
+  const match = valueStringMayHaveUnit.match(/^([0-9.]+)([a-zA-Z%]*)$/);
+  if (!match)
+    throw Error(
+      "Value string does not match expected format: " + valueStringMayHaveUnit,
+    );
+
+  const valueString = match[1];
+  if (!valueString) throw Error("Value string is empty");
+
+  const value = Number.parseFloat(valueString);
+
+  const unit = match[2] || null; // 単位がない場合はnull
+  return { value, unit };
+};
