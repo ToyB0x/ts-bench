20c12ab4bbde98f932d593feafeb8820ed1fbb3b
ToyB0x <83337042+ToyB0x@users.noreply.github.com>
2025-06-24 08:19:44 +0900

    feat: update loader to use select method for package results and improve query structure

--- apps/web/app/routes/graph.$name.tsx
+++ apps/web/app/routes/graph.$name.tsx
@@ -1,4 +1,4 @@
-import { db } from "@ts-bench/db";
+import { db, eq, resultTbl, scanTbl } from "@ts-bench/db";
 import type { Route } from "./+types/graph.$name";
 
 // biome-ignore lint/correctness/noEmptyPattern: example code
@@ -10,30 +10,30 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export async function loader({ params }: Route.LoaderArgs) {
-  return db.query.resultTbl.findMany({
-    where: (table, { eq }) =>
-      eq(table.package, `${params.scope}/${params.name}`),
-    with: {
-      scan: {
-        columns: {
-          commitHash: true,
-          commitMessage: true,
-          scannedAt: true,
-        },
-      },
-    },
-    orderBy: (table, { asc }) => asc(table.scanId),
-    limit: 100,
-  });
+  const packageFullName = params.scope
+    ? `${params.scope}/${params.name}`
+    : params.name;
+
+  const packageResults = await db
+    .select()
+    .from(resultTbl)
+    .where(eq(resultTbl.package, packageFullName))
+    .innerJoin(scanTbl, eq(resultTbl.scanId, scanTbl.id))
+    .orderBy(scanTbl.commitDate)
+    .limit(300);
+
+  return {
+    packageFullName,
+    results: packageResults,
+  };
 }
 
 export default function Page({ loaderData }: Route.ComponentProps) {
-  const results = loaderData;
-  const name = results[0]?.package || "Unknown Package";
+  const { results, packageFullName } = loaderData;
 
   return (
     <div>
-      <h1 className="bg-blue-400">{name}</h1>
+      <h1 className="bg-blue-400">{packageFullName}</h1>
       <pre>{JSON.stringify(results)}</pre>
     </div>
   );
